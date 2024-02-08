daafdb2c53e7e941133e261f07ce416f7f740ea5
Tangyuan <zhk3801258@outlook.com>
2024-02-08 21:07:23 +0800

    change to python test file

---
+++ test.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    print("Hello, your Python environment is working!")
+    print("Python version info:")
+    print(sys.version)
+
+if __name__ == "__main__":
+    main()
